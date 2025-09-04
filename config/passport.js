b47372ae06c6d46af396344f31f7d6f2b16040f6
Parth0603 <parthnagar2345@gmail.com>
2025-09-04 14:28:53 +0530

    added frontend url

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/api/auth/google/callback'
+  callbackURL: process.env.BACKEND_URL ? `${process.env.BACKEND_URL}/api/auth/google/callback` : 'http://localhost:5000/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ email: profile.emails[0].value });
